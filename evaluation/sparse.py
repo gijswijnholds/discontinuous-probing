75761621c205b18404ed95c73baaf627d51260c2
konstantinosKokos <konstantinos@riseup.net>
2021-10-07 20:09:40 +0200

    add cartesian mask

--- evaluation/sparse.py
+++ evaluation/sparse.py
@@ -33,24 +33,25 @@ def find_matches(bert_outputs: Tensor,
     offsets = offsets.cumsum(dim=-1).roll(1, 0)     # (batch,)
     offsets[0] = 0                                  # (batch,)                  -- start of each sentence
     sparse_batch = bert_outputs[mask != 0]          # (num_tokens, dim))        -- token reprs for the entire batch
-    n_batch, n_id, n_source = sparse_spans(n_spanss, offsets)
-    v_batch, v_id, v_source = sparse_spans(v_spans, offsets)
+    n_batch, n_id, n_source, num_n = sparse_spans(n_spanss, offsets)
+    v_batch, v_id, v_source, num_v = sparse_spans(v_spans, offsets)
     n_reprs = placeholder_aggregation(
         sparse_batch[n_source], n_id)               # (num_nouns, dim)          -- aggregated noun representations
     v_reprs = placeholder_aggregation(
         sparse_batch[v_source], v_id)               # (num_verbs, dim)          -- aggregated verb representations
-    vn_mask: Tensor = ...                           # (num_verbs, num_nouns)    -- verb to noun attention
+    v_mask, n_mask = torch.cartesian_prod(v_batch, n_batch).chunk(2, dim=-1)
+    vn_mask = v_mask.eq(n_mask).view(num_v, num_n)  # (num_verbs, num_nouns)    -- verb to noun alignment
     x_atn: Tensor = ...                             # (num_verbs, num_nouns)    -- verb to noun attention
     x_atn[vn_mask.eq(0)] = -1e10
     return x_atn.softmax(dim=-1)
 
 
-def sparse_spans(spanss: list[list[list[int]]], offsets: Sequence[int]) -> tuple[Tensor, Tensor, Tensor]:
+def sparse_spans(spanss: list[list[list[int]]], offsets: Sequence[int]) -> tuple[Tensor, Tensor, Tensor, int]:
     split = [(batch, span) for batch, spans in enumerate(spanss) for span in spans]
     batch_idx, token_idx, spans = list(zip(*[
         (b, torch.ones(sum(span), dtype=torch.long) * idx, torch.tensor(span).nonzero().squeeze(-1) + offsets[b])
         for idx, (b, span) in enumerate(split)]))
-    return torch.tensor(batch_idx), torch.cat(token_idx), torch.cat(spans)
+    return torch.tensor(batch_idx), torch.cat(token_idx), torch.cat(spans), len(split)
 
 
 def batch_matches(n_spans: list[list[list[int]]], v_spans: list[list[list[int]]], labels: list[list[int]]) -> Tensor:
