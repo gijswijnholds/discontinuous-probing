847c33c02cd233e6c0765ef3e86dc792e3069030
konstantinosKokos <konstantinos@riseup.net>
2021-10-07 13:36:07 +0200

    change dtypes and update to new format

--- evaluation/attention_grammarreader.py
+++ evaluation/attention_grammarreader.py
@@ -1,10 +1,9 @@
-from typing import List, Tuple
 from typing import Optional as Maybe
-import pickle
+from typing import Union, NamedTuple
 from pathlib import Path
 import json
 
-
+Matching = dict[int, int]
 Realized = list[tuple[list[int], list[int], str]]
 example_fn = './data/grammars/example_control.p'
 # train_example_fn = './data/grammars/example_cluster_train.txt'
@@ -13,66 +12,64 @@ example_fn = './data/grammars/example_control.p'
 train_example_fn = './data/grammars/example_control_train.txt'
 dev_example_fn = './data/grammars/example_control_dev.txt'
 test_example_fn = './data/grammars/example_control_test.txt'
-CompactSamples = tuple[List[str], List[List[int]], List[List[int]], int]
 
 
-def fix_matching(matching: dict[str, int]) -> dict[int, int]:
-    return {int(k):matching[k] for k in matching}
+AbsTree = Union[tuple[Maybe[int], Maybe[int], str], tuple['AbsTree', ...]]
 
 
-def open_grammar(fn: Path):
+class CompactSample(NamedTuple):
+    depth: int
+    abstree: AbsTree
+    sentence: list[str]
+    n_spans: list[list[int]]
+    v_spans: list[list[int]]
+    labels: list[int]
+
+
+def fix_matching(matching: dict[str, int]) -> Matching:
+    return {int(k): matching[k] for k in matching}
+
+
+def open_grammar(fn: Path) -> list[tuple[int, AbsTree, Matching, Realized]]:
     with open(fn, 'r') as inf:
-        data = [json.loads(ln) for ln in inf.readlines()]
-    realizations, matchings = zip(*[(d['surfaces'], fix_matching(d['matching'])) for d in data])
-    return realizations, matchings
+        data = json.load(inf)
+    return [(int(depth), abstree, fix_matching(data[depth][abstree][0]), eval(surface))
+            for depth in data for abstree in data[depth] for surface in data[depth][abstree][1]]
 
 
-def get_span(constant: str, idx: Maybe[int]) -> List[int]:
+def get_span(constant: str, idx: Maybe[int]) -> list[int]:
     return len(constant.split()) * [idx]
 
 
-def get_full_span(wss: List[str], idss: List[List[int]], idx: int):
+def get_full_span(wss: list[str], idss: list[list[int]], idx: int):
+    """
+    ["de socialist", "eet", "een appel"] -> [0, 0, None, 1, 1]
+                                        -> [[1, 1, 0, 0, 0], [0, 0, 0, 1, 1]]
+    """
     return sum([get_span(ws, idx if idx in ids else None) for (ids, ws) in zip(idss, wss)], [])
 
 
-def correct_indices(samples: CompactSamples, special_idx: int) -> CompactSamples:
+def correct_indices(depth: int, abstree: AbsTree, sentence: list[str], noun_spans: list[list[int]],
+                    verb_spans: list[list[int]], labels: list[int]) -> CompactSample:
     """Replace None indices by 0 and offset all other indices"""
-    sentence, noun_spans, verb_items = samples
-    noun_spans_out = list(map(lambda span: [0 if i is None else i+1 for i in span], noun_spans))
-    verb_items_out = list(map(lambda span_label: ([0 if v is None else special_idx for v in span_label[0]],
-                                                  span_label[1]+1), verb_items))
-    return sentence, noun_spans_out, verb_items_out
+    noun_spans_out = list(map(lambda span: [0 if i is None else 1 for i in span], noun_spans))
+    verb_spans_out = list(map(lambda span: [0 if v is None else 1 for v in span], verb_spans))
+    labels_out = list(map(lambda label: label+1, labels))
+    return CompactSample(depth, abstree, sentence, noun_spans_out, verb_spans_out, labels_out)
 
 
-def realization_to_sequences(realization: Realized, matching: dict[int, int], special_idx: int)\
-        -> CompactSamples:
+def make_sample(depth: int, abstree: AbsTree, matching: Matching, realization: Realized) -> CompactSample:
     """Given a realization (a list of constituents with their noun/verb indications, a matching from verbs to nouns,
     and the special index for the verb, we generate multiple data samples (one for each verb), for the model to train
     on. The format is: (sentence, noun spans, [(verb_span, label), ...])"""
     nss, vss, wss = zip(*realization)
-    sentence = ' '.join(wss)
     n_ids = set(sum(nss, []))
-    v_ids = set(sum(vss, []))
     noun_spans = list(map(lambda ni: get_full_span(wss, nss, ni), n_ids))
-    verb_items = list(map(lambda k: (get_full_span(wss, vss, k), matching[k]), matching))
-    compact_samples = (sentence, noun_spans, verb_items)
-    return correct_indices(compact_samples, special_idx)
+    verb_spans = list(map(lambda k: get_full_span(wss, vss, k), matching))
+    labels = list(map(lambda k: matching[k], matching))
+    return correct_indices(depth, abstree, wss, noun_spans, verb_spans, labels)
 
 
-def sample_to_datas(sample: CompactSamples) -> list[dict]:
-    sentence, noun_spans, verb_items = sample
-    return [{'sentence': sentence,
-             'noun_spans': noun_spans,
-             'verb_span': verb_span,
-             'label': label}
-            for (verb_span, label) in verb_items]
-
-
-def real_match_to_datas(real: Realized, match: dict[int, int], verb_idx: int):
-    return sum(map(lambda r: sample_to_datas(realization_to_sequences(r, match, special_idx=verb_idx)), real), [])
-
-
-def grammar_to_dataset_format(grammar_fn: Path) -> List:
-    realized, matchings = open_grammar(grammar_fn)
-    verb_idx = 99
-    return sum(map(lambda real_match: real_match_to_datas(*real_match, verb_idx), zip(realized, matchings)), [])
+def grammar_to_dataset_format(grammar_fn: Path) -> list[CompactSample]:
+    samples = open_grammar(grammar_fn)
+    return list(map(lambda s: make_sample(*s), samples))
