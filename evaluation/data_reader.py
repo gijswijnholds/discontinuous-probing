e15066c25d38fecbc7ab083bc29be10819e78f5d
konstantinosKokos <konstantinos@riseup.net>
2021-10-11 15:17:53 +0200

    fix multiplying spans by word len (!)

--- evaluation/data_reader.py
+++ evaluation/data_reader.py
@@ -29,32 +29,18 @@ def fix_matching(matching: dict[str, int]) -> Matching:
     return {int(k): matching[k] for k in matching}
 
 
-def open_grammar(fn: str) -> list[tuple[int, AbsTree, Matching, Realized]]:
-    with open(fn, 'r') as inf:
-        data = json.load(inf)
-    return [(int(depth), abstree, fix_matching(data[depth][abstree][0]), eval(surface))
-            for depth in data for abstree in data[depth] for surface in data[depth][abstree][1]]
+def process_grammar(
+        data: dict[str, dict[str, dict[AbsTree, tuple[dict[str, int], list[str]]]]]) \
+        -> list[list[tuple[int, AbsTree, Matching, Realized]]]:
+    return [[(int(depth), abstree, fix_matching(data[subset][depth][abstree][0]), eval(surface))
+             for depth in data[subset]
+             for abstree in data[subset][depth]
+             for surface in data[subset][depth][abstree][1]]
+            for subset in data]
 
 
-def get_span(constant: str, idx: Maybe[int]) -> list[int]:
-    return len(constant.split()) * [idx]
-
-
-def get_full_span(wss: list[str], idss: list[list[int]], idx: int):
-    """
-    ["de socialist", "eet", "een appel"] -> [0, 0, None, 1, 1]
-                                        -> [[1, 1, 0, 0, 0], [0, 0, 0, 1, 1]]
-    """
-    return sum([get_span(ws, idx if idx in ids else None) for (ids, ws) in zip(idss, wss)], [])
-
-
-def correct_indices(depth: int, abstree: AbsTree, sentence: list[str], noun_spans: list[list[int]],
-                    verb_spans: list[list[int]], labels: list[int]) -> CompactSample:
-    """Replace None indices by 0 and offset all other indices"""
-    noun_spans_out = list(map(lambda span: [0 if i is None else 1 for i in span], noun_spans))
-    verb_spans_out = list(map(lambda span: [0 if v is None else 1 for v in span], verb_spans))
-    labels_out = list(map(lambda label: label+1, labels))
-    return CompactSample(depth, abstree, sentence, noun_spans_out, verb_spans_out, labels_out)
+def expand_spans(idss: list[list[int]], idx: int):
+    return [1 if idx in ids else 0 for ids in idss]
 
 
 def make_sample(depth: int, abstree: AbsTree, matching: Matching, realization: Realized) -> CompactSample:
@@ -63,12 +49,19 @@ def make_sample(depth: int, abstree: AbsTree, matching: Matching, realization: R
     on. The format is: (sentence, noun spans, [(verb_span, label), ...])"""
     nss, vss, wss = zip(*realization)
     n_ids = set(sum(nss, []))
-    noun_spans = list(map(lambda ni: get_full_span(wss, nss, ni), n_ids))
-    verb_spans = list(map(lambda k: get_full_span(wss, vss, k), matching))
-    labels = list(map(lambda k: matching[k], matching))
-    return correct_indices(depth, abstree, wss, noun_spans, verb_spans, labels)
+    v_ids = set(sum(vss, []))
+    noun_spans = list(map(lambda ni: expand_spans(nss, ni), n_ids))
+    verb_spans = list(map(lambda vi: expand_spans(vss, vi), v_ids))
+    labels_out = list(map(lambda k: matching[k], matching))
+    return CompactSample(depth, abstree, wss, noun_spans, verb_spans, labels_out)
 
 
-def read_grammar(grammar_fn: str) -> list[CompactSample]:
-    samples = open_grammar(grammar_fn)
-    return list(map(lambda s: make_sample(*s), samples))
+def makes_samples(subsets: list[list[tuple[int, AbsTree, Matching, Realized]]]) -> list[list[CompactSample]]:
+    return [[make_sample(*s) for s in subset] for subset in subsets]
+
+
+def read_grammar(grammar_fn: str) -> list[list[CompactSample]]:
+    with open(grammar_fn, 'r') as inf:
+        data = json.load(inf)
+    return makes_samples(process_grammar(data))
+
