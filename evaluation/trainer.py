fd9764e88c20b3bd547b8253ee952b443dc7c924
konstantinosKokos <konstantinos@riseup.net>
2021-10-11 16:29:04 +0200

    allow skipping epochs

--- evaluation/trainer.py
+++ evaluation/trainer.py
@@ -14,12 +14,13 @@ def sequence_collator(word_pad: int) -> \
 
     def collate_fn(samples: list[ProcessedSample]) -> \
             tuple[Tensor, Tensor, list[list[list[int]]], list[list[list[int]]], Tensor]:
-        input_ids = pad_sequence([torch.tensor(sample) for sample in samples], padding_value=word_pad, batch_first=True)
+        input_ids = pad_sequence([torch.tensor(sample.tokens) for sample in samples],
+                                 padding_value=word_pad, batch_first=True)
         input_mask = input_ids != word_pad
         verb_spanss = [sample.verb_spans for sample in samples]
         noun_spanss = [sample.noun_spans for sample in samples]
         labelss = [sample.compact.labels for sample in samples]
-        return input_mask, input_ids, verb_spanss, noun_spanss, sparse_matches(noun_spanss, labelss)
+        return input_ids, input_mask, verb_spanss, noun_spanss, sparse_matches(noun_spanss, labelss)
     return collate_fn
 
 
@@ -111,52 +112,19 @@ class Trainer:
                 epoch_accuracy += accuracy
         return epoch_loss / len(loader), epoch_accuracy / len(loader)
 
-    # @no_grad()
-    # def predict_batch(self, input_ids: LongTensor, input_masks: LongTensor,
-    #                   verb_tags: LongTensor, candidate_masks: LongTensor, candidate_tags: List[LongTensor],
-    #                   ys: LongTensor):
-    #     self.model.eval()
-    #
-    #     predictions = self.model.forward(input_ids, input_masks, verb_tags, candidate_masks, candidate_tags)
-    #     batch_loss = self.loss_fn(predictions, ys)
-    #     accuracy = compute_accuracy(predictions, ys)
-    #
-    #     return batch_loss.item(), accuracy, input_ids, predictions
-    #
-    # def predict_epoch(self, eval_set: str, device: str, epoch_i: int):
-    #     epoch_loss, epoch_accuracy = 0., 0.
-    #     epoch_input_ids, epoch_predictions, epoch_ys = [], [], []
-    #     loader = {'train': self.train_loader, 'val': self.val_loader, 'test': self.test_loader}[eval_set]
-    #     batch_counter = 0
-    #     with tqdm(loader, unit="batch") as tepoch:
-    #         for input_ids, input_masks, verb_tags, candidate_masks, candidate_tags, ys in tepoch:
-    #             tepoch.set_description(f"Epoch {epoch_i}")
-    #             batch_counter += 1
-    #             input_ids.to(device)
-    #             input_masks.to(device)
-    #             verb_tags.to(device)
-    #             candidate_masks.to(device)
-    #             [tags.to(device) for tags in candidate_tags]
-    #             ys.to(device)
-    #             loss, accuracy, input_ids, predictions = self.predict_batch(input_ids, input_masks, verb_tags, candidate_masks, candidate_tags, ys)
-    #             tepoch.set_postfix(loss=loss, accuracy=accuracy.item())
-    #             epoch_loss += loss
-    #             epoch_input_ids += input_ids
-    #             epoch_accuracy += accuracy
-    #             epoch_predictions += predictions
-    #             epoch_ys += ys
-    #     return epoch_loss / len(loader), epoch_accuracy / len(loader), epoch_input_ids, epoch_predictions, epoch_ys
-
-    def main_loop(self, num_epochs: int, device: str):
+    def main_loop(self, num_epochs: int, device: str, val_every: int = 1):
         results = {i+1: {} for i in range(num_epochs)}
         for e in range(num_epochs):
             print(f"Epoch {e+1}...")
             train_loss, train_acc = self.train_epoch(device=device, epoch_i=e+1)
             print(f"Train loss {train_loss:.5f}, Train accuracy: {train_acc:.5f}")
-            val_loss, val_acc = self.eval_epoch(eval_set='val', device=device, epoch_i=e+1)
-            print(f"Val loss {val_loss:.5f}, Val accuracy: {val_acc:.5f}")
-            test_loss, test_acc = self.eval_epoch(eval_set='test', device=device, epoch_i=e+1)
-            print(f"Test loss {test_loss:.5f}, Test accuracy: {test_acc:.5f}")
+            if e % val_every == 0:
+                val_loss, val_acc = self.eval_epoch(eval_set='val', device=device, epoch_i=e+1)
+                print(f"Val loss {val_loss:.5f}, Val accuracy: {val_acc:.5f}")
+                test_loss, test_acc = self.eval_epoch(eval_set='test', device=device, epoch_i=e+1)
+                print(f"Test loss {test_loss:.5f}, Test accuracy: {test_acc:.5f}")
+            else:
+                val_loss, val_acc, test_loss, test_acc = None, None, None, None
             results[e+1] = {'train_loss': train_loss, 'train_acc': train_acc,
                             'val_loss': val_loss, 'val_acc': val_acc,
                             'test_loss': test_loss, 'test_acc': test_acc}
