0404e48c632d11eceefe5ff22adc378e6dc28fea
konstantinosKokos <konstantinos@riseup.net>
2021-10-14 18:06:01 +0200

    Changes by gijswijnholds

--- evaluation/trainer.py
+++ evaluation/trainer.py
@@ -129,7 +129,7 @@ class Trainer:
             print(f"Epoch {e+1}...")
             train_loss, train_acc = self.train_epoch(epoch_i=e+1)
             print(f"Train loss {train_loss:.5f}, Train accuracy: {train_acc:.5f}")
-            if (e % val_every == 0 and e != 0) or e == (num_epochs - 1):
+            if ((e+1) % val_every == 0 and e != 0) or e == (num_epochs - 1):
                 val_loss, val_acc = self.eval_epoch(eval_set='val', epoch_i=e+1)
                 print(f"Val loss {val_loss:.5f}, Val accuracy: {val_acc:.5f}")
                 test_preds = self.predict_epoch()
