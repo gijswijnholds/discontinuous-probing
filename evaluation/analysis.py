9fc9a54c766ea60c85996e75e853712a08c64f39
konstantinosKokos <konstantinos@riseup.net>
2021-10-15 13:53:43 +0200

    Changes by gijswijnholds

--- evaluation/analysis.py
+++ evaluation/analysis.py
@@ -1,10 +1,11 @@
 from .preprocessing import CompactSample, SpanDataset
 from itertools import groupby
 from typing import Any
+import pickle
+from collections import defaultdict
 
 
-def analysis(experiment, verbose: bool = True):
-    def show(whatever: str): print(whatever) if verbose else None
+def analysis(experiment):
     epochs = {k: v for k, v in experiment.items() if isinstance(k, int)}
     test_data: SpanDataset = experiment['test_data']
     best_epoch = max(epochs, key=lambda k: epochs[k]['val_acc'])
@@ -13,18 +14,14 @@ def analysis(experiment, verbose: bool = True):
     correct, total, _ = list(zip(*gbd.values()))
     def sort_by_acc(xs): return sorted(xs.items(), key=lambda x: -x[1][-1])
     def sort_by_key(xs): return sorted(xs.items(), key=lambda x: x[0])
-    print('Accuracy by depth:')
-    print('\n'.join([f'{d}:\t{c}\t{t}\t({a:0.2f})' for d, (c, t, a) in sort_by_key(gbd)]))
-    print('=' * 64)
-    print('Acccuracy by verb:')
-    print('\n'.join([f'{d}:\t{c}\t{t}\t({a:0.2f})' for d, (c, t, a) in sort_by_acc(gbv)]))
-    print('=' * 64)
-    print('Acccuracy by number of nouns:')
-    print('\n'.join([f'{d}:\t{c}\t{t}\t({a:0.2f})' for d, (c, t, a) in sort_by_key(gbn)]))
-    print('=' * 64)
-    print(f'Baseline accuracy: ({round(baseline(predictions), 2)})')
-    print('=' * 64)
-    print(f'Total accuracy:\t{sum(correct)}\t{sum(total)}\t({round(sum(correct)/sum(total), 2)})')
+    results = dict()
+    results['acc_by_depth'] = sort_by_key(gbd)
+    results['acc_by_verb'] = sort_by_acc(gbv)
+    results['acc_by_num_nouns'] = sort_by_key(gbn)
+    results['baseline'] = baseline(predictions)
+    results['total'] = (sum(correct), sum(total), sum(correct)/sum(total))
+    results['best_epoch'] = best_epoch
+    return results
 
 
 def verb_depth_acc(pss: list[list[int]], samples: list[CompactSample]) \
@@ -49,4 +46,60 @@ def avg(ln):
     return sum(ln) / len(ln)
 
 
-def aggregate(file):
+def get_group_agg(results: list, field: str):
+    sort_results = sorted([d_res for res in results for d_res in res[field]], key=lambda r: r[0])
+    grouped_results = {k: [v[1] for v in vs] for k, vs in groupby(sort_results, key=lambda r: r[0])}
+    avg_results = {k: tuple(map(avg, zip(*vs))) for k, vs in grouped_results.items()}
+    return avg_results
+
+
+def merge_results(all_results: list[dict]) -> dict:
+    merged_results = dict()
+
+    merged_results['acc_by_depth'] = get_group_agg(all_results, 'acc_by_depth')
+    merged_results['acc_by_verb'] = get_group_agg(all_results, 'acc_by_verb')
+    merged_results['acc_by_num_nouns'] = get_group_agg(all_results, 'acc_by_num_nouns')
+    merged_results['baseline'] = avg([res['baseline'] for res in all_results])
+    merged_results['total'] = tuple(map(avg, zip(*[res['total'] for res in all_results])))
+    merged_results['best_epochs'] = [res['best_epoch'] for res in all_results]
+    return merged_results
+
+
+def aggregate(file: str):
+    with open(file, 'rb') as in_file:
+        data = pickle.load(in_file)
+    agg_results = defaultdict(list)
+    for experiment in data:
+        exp_name, data_seed = experiment.split(':')[1].split('_')
+        agg_results[(exp_name, int(data_seed))] = [analysis(data[experiment][repeat])
+                                                   for repeat in data[experiment]]
+    return {k: merge_results(agg_results[k]) for k in agg_results}
+
+
+def show_results(results: dict):
+    names, data_seeds = list(zip(*results.keys()))
+    names, data_seeds = set(names), set(data_seeds)
+    for name in names:
+        print(name)
+        print('=' * 64)
+
+        d_accs = [{'acc_by_depth': results[(name, dseed)]['acc_by_depth']} for dseed in data_seeds]
+        n_accs = [{'acc_by_num_nouns': results[(name, dseed)]['acc_by_num_nouns']} for dseed in data_seeds]
+        agg_acc_by_depth = get_group_agg(d_accs, 'acc_by_depth')
+        agg_acc_by_num_nouns = get_group_agg(n_accs, 'acc_by_num_nouns')
+        agg_baseline = avg([results[(name, data_seed)]['baseline'] for data_seed in data_seeds]),
+        correct, total, _ = tuple(map(avg, zip(*[results[(name, data_seed)]['total'] for data_seed in data_seeds])))
+        best_epochs = [best_epoch for dseed in data_seeds for best_epoch in results[(name, dseed)]['best_epochs']]
+        print('=' * 64)
+        print('Accuracy by depth:')
+        print('\n'.join([f'{d}:\t{c}\t{t}\t({a:0.2f})' for d, (c, t, a) in agg_acc_by_depth.items()]))
+        print('=' * 64)
+        print('Accuracy by number of nouns:')
+        print('\n'.join([f'{d}:\t{c}\t{t}\t({a:0.2f})' for d, (c, t, a) in agg_acc_by_num_nouns.items()]))
+        print('=' * 64)
+        print('Best epochs:')
+        print(best_epochs)
+        print('Aggregated baseline:')
+        print(agg_baseline)
+        print('Average accuracy:')
+        print({correct/total})
